82b5cedcb9c08cedf1aae78983b6334373e0aed2
Jeremy Fernandez <jeremyfr101@gmail.com>
2023-04-24 14:32:41 -0400

    changed phrase from homepage

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>We are currently working on our app...</Text>
       <StatusBar style="auto" />
     </View>
   );
